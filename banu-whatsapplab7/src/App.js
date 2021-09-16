23861ec81941019fe2f87b450a5dbcc0313f4e0d
midoriobana <ingridmidori@live.com>
2021-09-16 00:05:22 -0300

    enviando mensagens

--- banu-whatsapplab7/src/App.js
+++ banu-whatsapplab7/src/App.js
@@ -1,5 +1,4 @@
-import react from 'react';
-import styled from 'styled-components'
+import React from "react";
 import Mensagem from "./Mensagem.js";
 
 
--- banu-whatsapplab7/src/Mensagem.js
+++ banu-whatsapplab7/src/Mensagem.js
@@ -1,9 +1,23 @@
 import React from "react";
 import styled from 'styled-components'
 
-const ContainerFundo = styled.div`
-    
-`
+const ContainerFundo = styled.div``
+const NomeUsuario = styled.span``;
+const Texto = styled.span``;
+
+const MensagemContainer = styled.div`
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 const ContainerVerde = styled.div`
     display: flex;
     flex-direction: row;
@@ -13,7 +27,6 @@ const ContainerVerde = styled.div`
     height: 10vh;
     background-color:#075E55;
 `
-
 
 const TituloBranco = styled.h2`
     color: #fff;
@@ -30,19 +43,33 @@ const ContainerInput = styled.div`
 
 class Mensagem extends React.Component {
     state = {
-    usuario: "",
-    mensagem: "",
+        listaMensagens: [],
+        usuario: "",
+        mensagem: ""
 };
 
-onChangeNome = (event) => {
-    this.setState({ usuario: event.target.value });
-  };
+    onChangeNome = (event) => {
+        this.setState({ usuario: event.target.value });
+    };
 
-  onChangeMensagem = (event) => {
-    this.setState({ mensagem: event.target.value });
-  };
+    onChangeMensagem = (event) => {
+        this.setState({ mensagem: event.target.value });
+    };
 
-  onClickEnviar = () => {};
+    onClickEnviar = () =>{
+        this.setState({usuario: "", mensagem:""});
+        const novaMensagem = [
+            ...this.state.listaMensagens,
+            {
+            usuario: this.state.usuario,
+            mensagem: this.state.mensagem
+        }
+    ];
+        this.setState({
+            listaMensagens: novaMensagem
+        });
+    }
+
     render() {
         return (
             <ContainerFundo>
@@ -61,9 +88,15 @@ onChangeNome = (event) => {
                     onChange={this.onChangeMensagem}
                 />
                 <button onClick={this.onClickEnviar}>Enviar</button>
+                {this.state.listaMensagens.map((mensagem, indice) => {
+                    return (
+                        <MensagemContainer>
+                          <NomeUsuario>{mensagem.usuario}</NomeUsuario>
+                          <Texto>{mensagem.mensagem}</Texto>
+                        </MensagemContainer>
+                      );
+                    })}
                 </ContainerInput>
-            
-                
             </ContainerFundo>
         );
     }
